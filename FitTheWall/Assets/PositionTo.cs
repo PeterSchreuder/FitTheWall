052f849eb2af53f378ba49dea4353b98067efdf8
Thomas Linssen <31830553+ThomasGITH@users.noreply.github.com>
2021-03-05 18:50:04 +0100

    Update PositionTo.cs
    changed comments again

--- FitTheWall/Assets/PositionTo.cs
+++ FitTheWall/Assets/PositionTo.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<summary> used to assign the two-dimensional (X and Y) coordinates to this object's transform </summary>
+/// <summary>
+/// used to assign the two-dimensional (X and Y) coordinates to this object's transform
+/// </summary>
 public class PositionTo : MonoBehaviour
 {
     //The object which coordinates are taken
@@ -13,7 +15,9 @@ public class PositionTo : MonoBehaviour
         AssignXYPosition();
     }
     
-<summary> Replaces X and Y coordinates with that of 'objectToTrack' </summary>
+ /// <summary>
+ /// Replaces X and Y coordinates with that of 'objectToTrack'
+ /// </summary>
     void AssignXYPosition()
     {
         gameObject.transform.position = new Vector3(objectToTrack.transform.position.x, objectToTrack.transform.position.y, gameObject.transform.position.z);
