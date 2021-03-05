d7e8712edbdc963ba902316e1e7203cbc92374fe
Thomas Linssen <t.linssen@live.nl>
2021-03-05 17:34:56 +0100

    cleaned up the code a bit, removed 'SilhouetteManager' script

--- FitTheWall/Assets/PositionTo.cs
+++ FitTheWall/Assets/PositionTo.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//used to assign the two-dimensional (X and Y) coordinates to this object's transform
 public class PositionTo : MonoBehaviour
 {
-    public GameObject objectToTrack;
+    [SerializeField] private GameObject objectToTrack;
+
     void Update()
     {
+        AssignXYPosition();
+    }
+
+    void AssignXYPosition()
+    {
+        //Replaces X and Y coordinates with that of 'objectToTrack'
         gameObject.transform.position = new Vector3(objectToTrack.transform.position.x, objectToTrack.transform.position.y, gameObject.transform.position.z);
     }
+
 }
